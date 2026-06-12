1ec3848ca74a3f173f778200699e92e6b23d4d3b
Jorge Washington Soares da Silva <jwss486@gmail.com>
2026-06-11 22:05:24 -0300

    adicionadas métodos de evio de email

--- src/test/java/com/jwss/studio/notificacao/NotificacaoApplicationTests.java
+++ src/test/java/com/jwss/studio/notificacao/NotificacaoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jwss.studio_notificacao;
+package com.jwss.studio.notificacao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class NotificacaoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
